47fcbbb592e83d94b1b0b24191b69f5ad3336ed0
Matheus Dario <dario.matheus97@gmail.com>
2023-10-08 00:11:32 -0300

    layout

---
+++ src/middlewares/middleware.js
@@ -0,0 +1,20 @@
+exports.middlewareGlobal = (req, res, next) => {
+    if(req.body.cliente) {
+        console.log()
+        console.log(`Vi que voce postou ${req.body.cliente}`)
+        console.log()
+    }
+    next()
+}
+
+exports.checkCsrfError = (err, req, res, next) => {
+    if(err) {
+        return res.render('404')
+    }
+    next()
+}
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken()
+    next()
+}
